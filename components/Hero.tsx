37430fbcaa7db94f011e70ca18c2ab51d178e42e
Mohit Motwani <mohitmotwani1999@gmail.com>
2026-02-13 12:20:57 +0400

    removed cringe element from hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -76,7 +76,6 @@ const Hero: React.FC = () => {
                   <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                   <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
                 </span>
-                <span className="text-stone-500 text-xs font-mono font-medium">System Online</span>
               </div>
             </div>
 
